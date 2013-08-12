70828dc5087b6084d43b89c6db3d3865e191518a
Juan Pablo Rivillas Ospina <jprivillaso@gmail.com>
2013-08-12 09:24:24 -0300

    Changed the architecture of the script, encapsulating fields

--- WebContent/js/Hierarchy.js
+++ WebContent/js/Hierarchy.js
@@ -1,4 +1,4 @@
-//Default position values for the 
+//Default position values for the margin
 var margin = {
 	top : 20,
 	right : 0,
@@ -6,23 +6,59 @@ var margin = {
 	left : 0
 }, width = 700, height = 400, formatNumber = d3.format(",d"), transitioning;
 
+//This method sets the properties of the text
+var text = function(text) {
+	//Position in x
+	text.attr("x", function(d) {
+		return x(d.x) + 6;
+	})
+	//Position in y
+	.attr("y", function(d) {
+		return y(d.y) + 25;
+	});
+};
+
+// This method sets the properties of the rectangle(node)
+var rect = function(rect) {
+	//Position in x
+	rect.attr("x", function(d) {
+		return x(d.x);
+	})
+	//Position in y
+	.attr("y", function(d) {
+		return y(d.y);
+	})
+	//Width of the node
+	.attr("width", function(d) {
+		return x(d.x + d.dx) - x(d.x);
+	})
+	//Height of the node
+	.attr("height", function(d) {
+		return y(d.y + d.dy) - y(d.y);
+	});
+};
+
+
+// This method returns the name of the actual node
+function name(d) {
+	return d.parent ? name(d.parent) + "." + d.name : d.name;
+}
 
 var x = d3.scale.linear().domain([ 0, width ]).range([ 0, width ]);
 var y = d3.scale.linear().domain([ 0, height ]).range([ 0, height ]);
 
-//Create the treemap to store the hierarchy
-var treemap = d3.layout
-	.treemap()
+//Create the Treemap to store the hierarchy
+var treemap = d3.layout.treemap()
 	//Select all the parents to display
 	.children(function(d, depth) {
 		return depth ? null : d.children;
 	})
-	//Organize automatically the nodes 
+	//Organize automatically the nodes depending the size
 	.sort(function(a, b) {
 		return a.value - b.value;
 	}).ratio(height / width * 0.5 * (1 + Math.sqrt(5))).round(false);
 
-//Create the svg with its properties. Create the hierarchical elements
+//Create the SVG with its properties. Create the hierarchical elements
 var svg = d3.select("#chart")
 	.append("svg")
 	.attr("width", width)
@@ -48,30 +84,25 @@ grandparent.append("text")
 	.attr("dy",".75em");
 
 //D3 JSON reader, it returns an array of objects. In this case is called root
-d3.json("test2.json", function(root) {
-	initialize(root);
-	accumulate(root);
-	layout(root);
-	display(root);
-	
+d3.json("test2.json", function(root) {	
 	//Initial values for the chart
-	function initialize(root) {
+	var initialize = function(root) {
 		root.x = root.y = 0;
 		root.dx = width;
 		root.dy = height;
 		root.depth = 0;
-	}
+	};
 
-	// This computes the values of all the children
-	function accumulate(d) {
+	//This computes the values of all the children
+	var accumulate = function(d) {
 		return d.children ? d.value = d.children.reduce(function(p, v) {
 			return p + accumulate(v);
 		}, 0) : d.value;
-	}
-
-	// Compute the treemap layout recursively such that each group of siblings
+	};
+	
+	// Compute the tree map layout recursively such that each group of siblings
 	// uses the same size rather than the dimensions of the parent cell.
-	function layout(d) {
+	var layout = function(d) {
 		if (d.children) {
 			treemap.nodes({
 				children : d.children
@@ -85,54 +116,12 @@ d3.json("test2.json", function(root) {
 				layout(c);
 			});
 		}
-	}
+	};
 	
 	// Display the tree map
-	function display(d) {
-		grandparent.datum(d.parent)
-			.on("click", transition)
-			.select("text")
-			.text(name(d));
-		
-		//Append grandparent
-		var g1 = svg.insert("g", ".grandparent")
-			.datum(d)
-			.attr("class", "depth");
-		
-		//Append Children
-		var g = g1.selectAll("g")
-			.data(d.children)
-			.enter()
-			.append("g");
-		
-		//Filter the displaying nodes
-		g.filter(function(d) {
-			return d.children;
-			}).classed("children", true)
-			.on("click", transition);
-
-		g.selectAll(".child")
-			.data(function(d) {
-				return d.children || [ d ];
-			}).enter()
-			.append("rect")
-			.attr("class", "child")
-			.call(rect);
-
-		g.append("rect")
-			.attr("class", "parent")
-			.call(rect);
-		
-		g.append("text")
-		.attr("dy", ".75em")
-		.text(function(d) {
-			return d.name + " - Value:" + d.value;
-		}).call(text)
-		.append("img")
-		.attr("src", 'http://www.seonightmares.com/images/good.png');
-		
+	var display = function(d) {
 		//This method is in charge of the zoom transition
-		function transition(d) {
+		var transition = function(d) {
 			if (transitioning || !d){
 				return;
 			}else{
@@ -169,35 +158,55 @@ d3.json("test2.json", function(root) {
 				svg.style("shape-rendering", "crispEdges");
 				transitioning = false;
 			});
-		}
-
+		};		
+		
+		grandparent.datum(d.parent)
+			.on("click", transition)
+			.select("text")
+			.text(name(d));
+		
+		//Append grandparent
+		var g1 = svg.insert("g", ".grandparent")
+			.datum(d)
+			.attr("class", "depth");
+		
+		//Append Children
+		var g = g1.selectAll("g")
+			.data(d.children)
+			.enter()
+			.append("g");
+		
+		//Filter the displaying nodes
+		g.filter(function(d) {
+			return d.children;
+			}).classed("children", true)
+			.on("click", transition);
+		
+		g.selectAll(".child")
+			.data(function(d) {
+				return d.children || [ d ];
+			}).enter()
+			.append("rect")
+			.attr("class", "child")
+			.call(rect);
+	
+		g.append("rect")
+			.attr("class", "parent")
+			.call(rect);
+		
+		//Create the text of each node, setting the height and the name that
+		// will be displayed on it
+		g.append("text")
+		.attr("dy", ".75em")
+		.text(function(d) {
+			return d.name + " - Value:" + d.value;
+		}).call(text);
+		
 		return g;
-	}
+	};
 	
-	//This method sets the properties of the text
-	function text(text) {
-		text.attr("x", function(d) {
-			return x(d.x) + 6;
-		}).attr("y", function(d) {
-			return y(d.y) + 25;
-		});
-	}
-
-	// This method sets the properties of the rectangle
-	function rect(rect) {
-		rect.attr("x", function(d) {
-			return x(d.x);
-		}).attr("y", function(d) {
-			return y(d.y);
-		}).attr("width", function(d) {
-			return x(d.x + d.dx) - x(d.x);
-		}).attr("height", function(d) {
-			return y(d.y + d.dy) - y(d.y);
-		});
-	}
-	
-	// This method returns the name of the actual node
-	function name(d) {
-		return d.parent ? name(d.parent) + "." + d.name : d.name;
-	}
+	initialize(root);
+	accumulate(root);
+	layout(root);
+	display(root);
 });
